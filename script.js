9bda480a86ec45c7868301507980e3bd1fa10126
Godwin <agblezeg@gmail.com>
2022-10-17 13:42:00 +0200

    Create array and object

---
+++ script.js
@@ -0,0 +1,12 @@
+let myLibrary = [];
+
+function Book(title, author, pages, status) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
+
+  this.info = function() {
+    return `${title} by ${author}, ${pages}, ${status}`;
+  }
+}
