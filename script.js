7fa9b3cd6d82134f26bbfabd8e482c0d32c7ced6
Godwin <agblezeg@gmail.com>
2022-10-19 16:51:01 +0200

    Add div to book information

--- script.js
+++ script.js
@@ -10,12 +10,8 @@ const booksContainer = document.querySelector(".books-container");
 function Book(title, author, pages, status) {
   this.title = title;
   this.author = author;
-  this.pages = pages;
+  this.pages = pages + " pages";
   this.status = status;
-
-  this.info = function() {
-    return `${title} by ${author}, ${pages}, ${status}`;
-  }
 }
 
 function addBookToLibrary() {
@@ -31,8 +27,9 @@ button.addEventListener("click", addBookToLibrary);
 
 function displayBook() {
   myLibrary.forEach(book => {
-    booksContainer.innerHTML += `<div class="book">${book.title} ${book.author} ${book.pages} ${book.status} <button class="delete">Delete</button> </div>`;
+    booksContainer.innerHTML += `<div class="book"> <div class="title">${book.title}</div> <div>${book.author}</div> <div>${book.pages}</div> <div>${book.status}</div> <button class="delete">Delete</button> </div>`;
     body.appendChild(booksContainer);
+    setDataAttribute();
   });
 }
 
