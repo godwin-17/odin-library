7fe614508b3adac220df60855e837c510b91f880
Godwin <agblezeg@gmail.com>
2023-06-06 15:58:16 +0200

    Add form controls

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ const inputStatus = document.querySelector("#status");
 const button = document.querySelector("button");
 const body = document.querySelector("body");
 const booksContainer = document.querySelector(".books-container");
+const form = document.querySelector("form");
+const errorTitle = document.querySelector(".error-title");
+const errorAuthor = document.querySelector(".error-author");
+const errorPages = document.querySelector(".error-pages");
+const errorStatus = document.querySelector(".error-status");
 
 function Book(title, author, pages, status) {
   this.title = title;
@@ -17,13 +22,58 @@ function Book(title, author, pages, status) {
 function addBookToLibrary() {
   let book = new Book(inputTitle.value, inputAuthor.value, inputPages.value, inputStatus.value);
   myLibrary.push(book);
-  console.log(myLibrary);
   booksContainer.innerHTML = "";
   displayBook();
   setDataAttribute();
 }
 
-button.addEventListener("click", addBookToLibrary);
+inputTitle.addEventListener("input", () => {
+  if (inputTitle.validity.valid) {
+    errorTitle.textContent = "";
+  } else {
+    showTitleError();
+  }
+});
+
+inputAuthor.addEventListener("input", () => {
+  if (inputAuthor.validity.valid) {
+    errorAuthor.textContent = "";
+  } else {
+    showAuthorError();
+  }
+});
+
+inputPages.addEventListener("input", () => {
+  if (inputPages.validity.valid) {
+    errorPages.textContent = "";
+  } else {
+    showPagesError();
+  }
+});
+
+inputStatus.addEventListener("input", () => {
+  if (inputStatus.validity.valid) {
+    errorStatus.textContent = "";
+  } else {
+    showStatusError();
+  }
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (!inputTitle.validity.valid) {
+    showTitleError();
+  } else if (!inputAuthor.validity.valid) {
+    showAuthorError();
+  } else if (!inputPages.validity.valid) {
+    showPagesError();
+  } else if (!inputStatus.validity.valid) {
+    showStatusError();
+  } else {
+    addBookToLibrary();
+  }
+});
 
 function displayBook() {
   myLibrary.forEach(book => {
@@ -33,7 +83,6 @@ function displayBook() {
   });
 }
 
-
 function setDataAttribute() {
   const books = document.querySelectorAll(".book");
   books.forEach((book, index) => {
@@ -41,11 +90,9 @@ function setDataAttribute() {
   });
 }
 
-// Delete books
 booksContainer.addEventListener("click", e => {
   if (e.target.classList.contains("delete")) {
     e.target.parentElement.remove();
-    console.log(e.target.parentElement.dataset.index);
     myLibrary.splice(e.target.parentElement.dataset.index, 1);
     setDataAttribute();
   }
@@ -60,3 +107,29 @@ booksContainer.addEventListener("click", e => {
     }
   }
 });
+
+function showTitleError() {
+  if (inputTitle.validity.valueMissing) {
+    errorTitle.textContent = "You need to enter a title";
+  }
+}
+
+function showAuthorError() {
+  if (inputAuthor.validity.valueMissing) {
+    errorAuthor.textContent = "You need to enter an author";
+  }
+}
+
+function showPagesError() {
+  if (inputPages.validity.valueMissing) {
+    errorPages.textContent = "You need to enter a number";
+  } else if (inputPages.validity.rangeUnderflow) {
+    errorPages.textContent = "Your book need to has at least 1 page";
+  }
+}
+
+function showStatusError() {
+  if (inputStatus.validity.valueMissing) {
+    errorStatus.textContent = "You need to enter a status";
+  }
+}
